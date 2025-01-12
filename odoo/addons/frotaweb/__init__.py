dea328fd2085cc0e113a6e8b16e4cdc9c218d58e
jcardus <asklocation.net@gmail.com>
2025-01-12 02:44:37 +0000

    start module

---
+++ odoo/addons/frotaweb/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'frotaweb',
+    'version': '0.1',
+    'summary': 'Main module',
+    'depends': [
+        'base_setup',
+        'sales_team',
+        'mail',
+        'calendar',
+        'resource',
+        'fetchmail',
+        'utm',
+        'web_tour',
+        'contacts',
+        'digest',
+        'phone_validation',
+    ],
+    'auto_install': True
+}
