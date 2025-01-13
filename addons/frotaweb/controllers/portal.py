63942bd9836bd0af8591a66a49f51f652260fa78
jcardus <asklocation.net@gmail.com>
2025-01-13 02:18:21 +0000

    logo small

--- addons/frotaweb/controllers/portal.py
+++
@@ -1,9 +0,0 @@
-from odoo.addons.portal.controllers.portal import CustomerPortal
-
-
-class PortalAccount(CustomerPortal):
-
-    def _prepare_portal_layout_values(self):
-        # EXTENDS 'portal'
-        portal_layout_values = super()._prepare_portal_layout_values()
-        return portal_layout_values
