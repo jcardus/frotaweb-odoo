8dea79997c28e16ccb274bde35ece87c85ae52bf
jcardus <asklocation.net@gmail.com>
2025-01-13 03:30:12 +0000

    dependes auth_oauth

--- addons/frotaweb/__manifest__.py
+++ addons/frotaweb/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '0.1',
     'summary': 'Main module',
     'depends': [
-        'account', 'portal'
+        'account', 'portal', 'auth_oauth'
     ],
     'auto_install': [],
     'application': True,
