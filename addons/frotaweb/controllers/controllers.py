d22a2b2163fc13ed2ed7d7ce05a33868c371dabe
jcardus <asklocation.net@gmail.com>
2025-01-17 21:03:29 +0000

    fix logout

--- addons/frotaweb/controllers/controllers.py
+++ addons/frotaweb/controllers/controllers.py
@@ -5,8 +5,24 @@ import random
 
 from odoo import http
 from odoo.http import request
+from werkzeug.utils import redirect
+from urllib.parse import quote
 
 logger = logging.getLogger(__name__)
+
+
+class CustomAuthLogout(http.Controller):
+    # Default Odoo login logic
+    @http.route('/web/session/logout', type='http', auth="user", website=True)
+    def logout(self, **kwargs):
+        request.session.logout()
+        base_url = request.httprequest.host_url
+        post_logout_redirect_uri = f"{base_url}web/login"
+        encoded_redirect_uri = quote(post_logout_redirect_uri, safe='')
+        auth0_logout_url = f"https://auth.pinme.io/logout?post_logout_redirect_uri={encoded_redirect_uri}"
+
+        return redirect(auth0_logout_url, 303)
+
 
 class Frotaweb(http.Controller):
     @http.route('/frotaweb/statistics', type='json', auth='user')
