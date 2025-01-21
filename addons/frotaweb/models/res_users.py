5fd7343ea93befaae4d651fccc72611b7e9a3f4d
jcardus <asklocation.net@gmail.com>
2025-01-21 02:21:21 +0000

    refactor odoo traccar

--- addons/frotaweb/models/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    traccar_token = fields.Char(string="Tracking platform token", help="Token for accessing tracking platform API")
